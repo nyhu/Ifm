75c9d07d04f0ca13da01ec727eee69ba9350d89e
nyhu <toussaint.boos@gmail.com>
2016-12-04 16:19:17 +0100

    pages controller and home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+
   devise_for :users, controllers: { sessions: 'users/sessions' }
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
+  root 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

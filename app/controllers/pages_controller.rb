a931b94f9f1bc0dbac5069305f48a81d066646bb
nyhu <toussaint.boos@gmail.com>
2016-12-16 10:08:16 +0100

    comments

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def home
+	@comments = Comment.all
+	@comments.each do |c|
+		c.title = User.find(c.user).pseudo.upcase
+	end
+	@comment = Comment.new
   end
 end
